0be4af046a13f3766702845a118110bd166d3a41
remsk <crystalsekai@gmail.com>
2024-04-01 01:09:06 -0400

    20240401

--- src/components/default/Share.vue
+++ src/components/default/Share.vue
@@ -176,7 +176,7 @@
                 }"
               >
                 <div
-                  v-if="dataStore.getStyle(style.id)[1] > 0"
+                  v-if="false && dataStore.getStyle(style.id)[1] > 0"
                   :style="{
                     position: `absolute`,
                     top: `0.0625rem`,
@@ -196,6 +196,28 @@
                 >
                   {{ `${dataStore.getStyle(style.id)[1]}` }}
                   <!-- <img :src="`/ui/ExchangeBadgeicon.webp`" :width="24" :height="24" /> -->
+                </div>
+                <div
+                  v-if="dataStore.getStyle(style.id)[1] > 0"
+                  :style="{
+                    position: `absolute`,
+                    top: `0.5rem`,
+                    left: `calc(50% - 20.5px)`,
+                    color: `black`,
+                    fontSize: `0.75rem`,
+                    lineHeight: `0.75rem`,
+                    // borderRadius: `50%`,
+                    width: `41.55555555555556px`,
+                    height: `12px`,
+                    textAlign: `center`,
+                    // clipPath: `polygon(45% 1.33975%, 46.5798% 0.60307%, 48.26352% 0.15192%, 50% 0%, 51.73648% 0.15192%, 53.4202% 0.60307%, 55% 1.33975%, 89.64102% 21.33975%, 91.06889% 22.33956%, 92.30146% 23.57212%, 93.30127% 25%, 94.03794% 26.5798%, 94.48909% 28.26352%, 94.64102% 30%, 94.64102% 70%, 94.48909% 71.73648%, 94.03794% 73.4202%, 93.30127% 75%, 92.30146% 76.42788%, 91.06889% 77.66044%, 89.64102% 78.66025%, 55% 98.66025%, 53.4202% 99.39693%, 51.73648% 99.84808%, 50% 100%, 48.26352% 99.84808%, 46.5798% 99.39693%, 45% 98.66025%, 10.35898% 78.66025%, 8.93111% 77.66044%, 7.69854% 76.42788%, 6.69873% 75%, 5.96206% 73.4202%, 5.51091% 71.73648%, 5.35898% 70%, 5.35898% 30%, 5.51091% 28.26352%, 5.96206% 26.5798%, 6.69873% 25%, 7.69854% 23.57212%, 8.93111% 22.33956%, 10.35898% 21.33975%)`,
+                    // textShadow: `1px 1px 0px #21212133`,
+                    filter: `drop-shadow(0px 0px 0px #000)`,
+                    // background: `linear-gradient(180deg, #ffffffaa 100%, #ffffff00 100%)`,
+                  }"
+                >
+                  <!-- {{ `${dataStore.getStyle(style.id)[1]}` }} -->
+                  <img :src="`/ui/ImageStarScore0${dataStore.getStyle(style.id)[1]}.webp`" :width="41.55555555555556" :height="12" />
                 </div>
                 <div v-if="dataStore.getStyle(style.id)[1] < 0" :style="{
                   width: `100%`,
