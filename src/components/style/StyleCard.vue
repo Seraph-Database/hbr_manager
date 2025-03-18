65090ee01be87b09c6be9285775edfa23c6813fd
remsk <crystalsekai@gmail.com>
2025-03-18 01:18:12 -0400

    added links to main site

--- src/components/style/StyleCard.vue
+++ src/components/style/StyleCard.vue
@@ -3,20 +3,31 @@
     class="style-placeholder"
     :style="{ width: `356px`, height: `144px`, background: `#999` }"
     @click.stop="dataStore.toggleStyle(style.id)"
+    v-ripple="{ class: 'text-white style-ripple' }"
   >
-    <img
-      width="356"
-      height="144"
-      class="d-block style-strip"
-      :src="`https://assets.hbr.quest/v1/hbr/${style.strip.replace(`Party`, `Select`)}`"
-    />
-    <style-header :style="style" />
-    <style-status v-if="!dataStore.readOnly" :style="style" />
+    <a
+      :href="`https://${String(route.name).endsWith(`En`) ? `en.` : ``}hbr.quest/styles/${style.label.toLowerCase()}`"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        width="356"
+        height="144"
+        class="d-block style-strip"
+        :src="`https://assets.hbr.quest/v1/hbr/${style.strip.replace(
+          `Party`,
+          `Select`
+        )}`"
+      />
+      <style-header :style="style" />
+      <style-status v-if="!dataStore.readOnly" :style="style" />
+    </a>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useStyleStore } from "@/store/app";
+import { useRoute } from "vue-router";
 import { type Style } from "@/types";
 import StyleHeader from "@/components/style/StyleHeader.vue";
 import StyleStatus from "@/components/style/StyleStatus.vue";
@@ -26,6 +37,7 @@ defineProps<{
   style: Style;
 }>();
 
+const route = useRoute();
 const dataStore = useStyleStore();
 // const route = useRoute();
 </script>
@@ -42,11 +54,17 @@ const dataStore = useStyleStore();
   overflow: hidden;
   user-select: none;
   position: relative;
+
+  a {
+    display: block;
+    text-decoration: none;
+  }
 }
 
 .style-ripple {
-  width: 100%;
-  max-width: 356px;
+  // width: 100%;
+  // max-width: 356px;
+  z-index: 1;
 }
 
 // .style-hexagon {
